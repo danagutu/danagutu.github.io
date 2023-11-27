98cd74a78cc15f9efb670bdbf0ee3dea30692abd
danagutu <danagutu2002@gmail.com>
2023-11-27 18:38:28 +0000

    configure FB (no API key)

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
 import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    // your config
-};
+    authDomain: "oit-mentorship-programme.firebaseapp.com",
+    databaseURL: "https://oit-mentorship-programme-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "oit-mentorship-programme",
+    storageBucket: "oit-mentorship-programme.appspot.com",
+    messagingSenderId: "688625353223",
+    appId: "1:688625353223:web:3d1c805941f96b8bc120a2",
+    measurementId: "G-X8WDFZV0M2"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
