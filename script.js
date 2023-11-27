6e3d295ad29e9a4fd6f10c9af2463d34948b3501
Charles Dupont <charles44.dupont@gmail.com>
2023-11-27 13:48:54 -0500

    Update to use firebase realtime database

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
-import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
+import { getDatabase, ref, get } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js';
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     authDomain: "oit-mentorship-programme.firebaseapp.com",
     databaseURL: "https://oit-mentorship-programme-default-rtdb.europe-west1.firebasedatabase.app",
@@ -10,22 +9,29 @@ const firebaseConfig = {
     messagingSenderId: "688625353223",
     appId: "1:688625353223:web:3d1c805941f96b8bc120a2",
     measurementId: "G-X8WDFZV0M2"
-  };
+};
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getDatabase(app);
 
 async function fetchEvents() {
+    const eventsRef = ref(db, 'events/');
     try {
-        const querySnapshot = await getDocs(collection(db, "events"));
+        const snapshot = await get(eventsRef);
         const eventsContainer = document.getElementById('events-container');
         eventsContainer.innerHTML = ''; // Clear existing content
-        querySnapshot.forEach((doc) => {
-            const eventData = doc.data();
-            const eventElement = document.createElement('div');
-            eventElement.textContent = `Event: ${eventData.name}, Date: ${eventData.date}`;
-            eventsContainer.appendChild(eventElement);
-        });
+
+        if (snapshot.exists()) {
+            const data = snapshot.val();
+            for (const eventId in data) {
+                const eventData = data[eventId];
+                const eventElement = document.createElement('div');
+                eventElement.textContent = `Event: ${eventData.name}, Date: ${eventData.date}`;
+                eventsContainer.appendChild(eventElement);
+            }
+        } else {
+            console.log("No data available");
+        }
     } catch (error) {
         console.error("Error fetching events: ", error);
     }
