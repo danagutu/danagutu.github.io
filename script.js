10d799a61a2ff6978c0bb3caf84c32976cb988b5
Charles Dupont <charles44.dupont@gmail.com>
2023-11-27 13:25:50 -0500

    Add basic HTML / CSS / JS structure

---
+++ script.js
@@ -0,0 +1,22 @@
+// TODO!
+
+// Initialize Cloud Firestore and get a reference to the service
+// const db = firebase.firestore();
+
+// function fetchEvents() {
+//     db.collection("events").get().then((querySnapshot) => {
+//         const eventsContainer = document.getElementById('events-container');
+//         eventsContainer.innerHTML = ''; // Clear existing content
+//         querySnapshot.forEach((doc) => {
+//             const eventData = doc.data();
+//             const eventElement = document.createElement('div');
+//             eventElement.textContent = `Event: ${eventData.name}, Date: ${eventData.date}`;
+//             eventsContainer.appendChild(eventElement);
+//         });
+//     }).catch((error) => {
+//         console.error("Error fetching events: ", error);
+//     });
+// }
+
+// // Fetch events when the script loads
+// fetchEvents();
