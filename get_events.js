f1bb8c059b24a4d2ab1446588eb752cebdf726ab
danagutu <danagutu2002@gmail.com>
2023-12-18 17:57:42 +0000

    only display events with free items

--- get_events.js
+++ get_events.js
@@ -31,65 +31,67 @@ async function fetchEvents() {
             for (const eventId in events) {
                 const eventData = events[eventId];
 
+                if (eventData.free_stuff.free_alch_drinks === true || eventData.free_stuff.free_food === true || eventData.free_stuff.free_soft_drinks === true) {
 
-                // Create the event card container
-                const eventCard = document.createElement('div');
-                eventCard.className = 'event-card';
-
-
-                // Create the card inner container
-                const cardInner = document.createElement('div');
-                cardInner.className = 'card-inner';
+                    // Create the event card container
+                    const eventCard = document.createElement('div');
+                    eventCard.className = 'event-card';
 
 
-                // Create the front face of the card (event image and title)
-                const cardFront = document.createElement('div');
-                cardFront.className = 'card-face';
-                cardFront.innerHTML += `
-                    <img src=${eventData.image} alt="Event Image">
-                    <h2>${eventData.title}</h2>
-                `;
+                    // Create the card inner container
+                    const cardInner = document.createElement('div');
+                    cardInner.className = 'card-inner';
 
 
-                // Create the back face of the card (event information)
-                const cardBack = document.createElement('div');
-                cardBack.className = 'card-face card-back';
-                cardBack.innerHTML += `
-                    <a class="event-link" href="${eventData.url}" target="_blank">${eventData.title}></a>
-                    <p>Date & Time: ${eventData.date_time}</p>
-                    <p>Location: ${eventData.location}</p>
-                `;
+                    // Create the front face of the card (event image and title)
+                    const cardFront = document.createElement('div');
+                    cardFront.className = 'card-face';
+                    cardFront.innerHTML += `
+                        <img src=${eventData.image} alt="Event Image">
+                        <h2>${eventData.title}</h2>
+                    `;
 
 
-                // Append the front and back faces to the card inner container
-                cardInner.appendChild(cardFront);
-                cardInner.appendChild(cardBack);
+                    // Create the back face of the card (event information)
+                    const cardBack = document.createElement('div');
+                    cardBack.className = 'card-face card-back';
+                    cardBack.innerHTML += `
+                        <a class="event-link" href="${eventData.url}" target="_blank">${eventData.title}></a>
+                        <p>Date & Time: ${eventData.date_time}</p>
+                        <p>Location: ${eventData.location}</p>
+                    `;
 
 
-                // Append the card inner container to the event card
-                eventCard.appendChild(cardInner);
+                    // Append the front and back faces to the card inner container
+                    cardInner.appendChild(cardFront);
+                    cardInner.appendChild(cardBack);
 
 
-                // Append the event card to the events container
-                eventsContainer.appendChild(eventCard);
+                    // Append the card inner container to the event card
+                    eventCard.appendChild(cardInner);
 
 
-            /*     // Add event listener to each card for hover effect
-                eventCard.addEventListener('mouseenter', () => {
-                    cardInner.style.transform = 'rotateY(180deg)';
-                });
+                    // Append the event card to the events container
+                    eventsContainer.appendChild(eventCard);
 
 
-                eventCard.addEventListener('mouseleave', () => {
-                    cardInner.style.transform = 'rotateY(0deg)';
-                });
-            } */
+                /*     // Add event listener to each card for hover effect
+                    eventCard.addEventListener('mouseenter', () => {
+                        cardInner.style.transform = 'rotateY(180deg)';
+                    });
 
 
-            // Add event listener to each card for click effect
-                eventCard.addEventListener('click', () => {
-                    cardInner.classList.toggle('flipped');
-                });
+                    eventCard.addEventListener('mouseleave', () => {
+                        cardInner.style.transform = 'rotateY(0deg)';
+                    });
+                } */
+
+
+                // Add event listener to each card for click effect
+                    eventCard.addEventListener('click', () => {
+                        cardInner.classList.toggle('flipped');
+                    });
+                }
             }
            
         } else {
